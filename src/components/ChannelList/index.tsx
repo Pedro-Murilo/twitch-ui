c9d2fd754b3a0e571512ba3d6481e22f48442835
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-01 19:14:06 -0300

    add: final project

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+  List,
+} from "./styles";
+
+const ChannelList: React.FC = () => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <Avatar />
+        <Column>
+          <Username>HunterRyoshi</Username>
+          <Info>2 new videos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  );
+
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
